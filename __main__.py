b232ce1182ae5169343c029ef7978bc8a9fdd59c
yuracoff18 <yurayt18@gmail.com>
2024-05-01 17:35:30 -0500

    uso de libreria st_pages

--- __main__.py
+++ __main__.py
@@ -1,10 +1,25 @@
 import streamlit as st
+from st_pages import *
+
+show_pages([
+    Page("__main__.py", "Home"),
+    Page("pages\compra.py", "Compra")
+])
+
+hide_pages([
+    "Compra"
+    
+    ])
 
 def main():
-    st.write("""
-# Hola Mundo
-Esta es una prueba
-""")
+    st.write(
+    """
+    # Piecitos
+    Compra al por mayor de zapatos
+    """)
+
+    if st.button("Comprar"):
+        st.switch_page("pages\compra.py")
 
 
 
---
+++ pages/compra.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("""
+         # Seccion de compra
+         Comprar zapatos?
+         """)
+
+if st.button("Volver"):
+    st.switch_page("__main__.py")
